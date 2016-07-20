cd5818aab4fa58ecdb651dd2efa7f2be76b4386d
pspeiser <pat@patspeiser.com>
2016-07-19 20:54:54 -0400

    git collab section

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
+var chalk = require('chalk');
+
+app.use( function(req, res, done){
+	console.log(chalk.red(req.method), chalk.blue(req.url))
+	done();
+});
 
 app.get('/', function(req, res){
 	res.send('yo');
@@ -7,7 +13,6 @@ app.get('/', function(req, res){
 
 app.get('/news', function(req, res){
 	res.send('you are at news');
-})
-
+});
 
 app.listen(3000);

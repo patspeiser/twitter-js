2340aba30aa4aee7cfa410d823821f54eef9e566
pspeiser <pat@patspeiser.com>
2016-07-19 21:24:08 -0400

    have swig rendering on /

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 var express = require('express');
 var app = express();
 var chalk = require('chalk');
+var swig = require('swig');
+
+app.engine('html', swig.renderFile)
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
 
 app.use( function(req, res, done){
 	console.log(chalk.red(req.method), chalk.blue(req.url))
 	done();
 });
 
-app.get('/', function(req, res){
-	res.send('yo');
+app.get('/', function(req, res) {
+		var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+	res.render('index', locals);
+	
 });
+
 
 app.get('/news', function(req, res){
 	res.send('you are at news');
 });
+
 
 app.listen(3000);
